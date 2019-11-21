3939e9554a626a55856f0c0881bbe27a42f58cc6
Jan Skrasek <hrach.cz@gmail.com>
2019-11-21 20:16:16 +0100

    ci: proper check if pushed to master

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if [[ $GITHUB_REF == "master" ]] ; then
+if [[ $GITHUB_REF == "refs/heads/master" ]] ; then
   php app/console.php get-menu --zomato-user-key=$ZOMATO_USER_KEY --slack-url=$SLACK_URL
 else
   php app/console.php get-menu --zomato-user-key=$ZOMATO_USER_KEY
